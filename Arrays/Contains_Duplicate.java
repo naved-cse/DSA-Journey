f2f16bf9352ebec47bcbc5ead58626709607ec87
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-07-06 21:14:28 +0530

    Added: Contains Duplicate  - LeetCode 217 - Easy

---
+++ Arrays/Contains_Duplicate.java
@@ -0,0 +1,28 @@
+// Problem: Contains Duplicate (LeetCode 217)
+// Difficulty: Easy
+// Approach: I used a HashSet to keep track of numbers already seen. While iterating through the array, if adding a
+// number to the set failed, it meant the number was already present, so I returned true immediately. If the loop
+// finished without finding duplicates, I returned false.
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        // creating a HashSet to keep track of numbers we have already seen
+        HashSet<Integer> seenNums = new HashSet<>();
+
+        // iterating through each number in the array
+        for (int num : nums) {
+
+            // trying to add the number to the set
+            // if add returns false, it means the number was already present
+            if (!seenNums.add(num)) {
+
+                // duplicate found, return true immediately
+                return true;
+            }
+        }
+
+        // if loop finishes without finding duplicates, return false
+        return false;
+    }
+}
