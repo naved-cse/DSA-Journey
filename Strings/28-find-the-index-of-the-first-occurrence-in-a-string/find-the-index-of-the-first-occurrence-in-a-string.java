1197dd32f7dc9bb468c1d833e8fc71293a9c7947
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-07-24 23:06:35 +0530

    Time: 1 ms (48.39%) | Memory: 43.1 MB (33.59%) - LeetSync

--- Strings/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ Strings/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,24 +1,33 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle.length() == 0) {
-            return 0;
-        }
+        // 1. Base cases
+        if (needle.length() == 0) return 0;
+        if (haystack.length() < needle.length()) return -1;
 
-        int n = haystack.length();
-        int m = needle.length();
-
-        for (int i = 0; i <= n - m; i++) {
-            int j = 0;
-
-            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+        // 2. THE ANCHOR LOOP
+        // We stop early because if the remaining letters are shorter 
+        // than the needle, it's physically impossible to find a match!
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            
+            int j = 0; // The Scout starts at the beginning of the needle
+            
+            // 3. THE SCOUT LOOP
+            while (j < needle.length()) {
+                // If there's a mismatch, sound the alarm and stop!
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break; 
+                }
+                // If it matched, the Scout takes a step forward
                 j++;
             }
 
-            if (j == m) {
-                return i;
+            // 4. Did the Scout make it through the entire needle?
+            if (j == needle.length()) {
+                return i; // We found the whole word! Return the Anchor's position.
             }
         }
 
+        // 5. The Anchor checked everywhere and found nothing
         return -1;
     }
 }
