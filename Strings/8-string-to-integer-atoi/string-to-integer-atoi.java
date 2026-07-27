a95913e5d957fe8a1db079fb84ad8d3b8ccb6146
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-07-27 22:45:58 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (97.52%) - LeetSync

--- Strings/8-string-to-integer-atoi/string-to-integer-atoi.java
+++ Strings/8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -5,31 +5,26 @@ class Solution {
         int sign = 1;
         int result = 0;
 
-        // Skip leading spaces
-        while (i < n && s.charAt(i) == ' ') {
+        while (i < n && s.charAt(i) == ' '){
             i++;
         }
 
-        if (i == n) {
+        if (i == n){
             return 0;
         }
 
-        // Check sign
-        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-            if (s.charAt(i) == '-') {
+        if (s.charAt(i) == '-' || s.charAt(i) == '+'){
+            if (s.charAt(i) == '-'){
                 sign = -1;
             }
             i++;
         }
 
-        // Read digits
-        while (i < n && Character.isDigit(s.charAt(i))) {
+        while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+
             int digit = s.charAt(i) - '0';
-
-            // Overflow check
-            if (result > Integer.MAX_VALUE / 10 ||
-                (result == Integer.MAX_VALUE / 10 && digit > 7)) {
-
+            
+            if (result > Integer.MAX_VALUE/10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
                 return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
 
