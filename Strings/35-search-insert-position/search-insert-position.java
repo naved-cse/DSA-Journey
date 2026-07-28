6775531a7de3d6313ece51b3ae56915bd317c773
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-07-29 01:35:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (83.54%) - LeetSync

--- Strings/35-search-insert-position/search-insert-position.java
+++ Strings/35-search-insert-position/search-insert-position.java
@@ -1,20 +1,30 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        // 1. Initialize left and right
         int left = 0;
         int right = nums.length - 1;
 
+        // 2. Loop while left <= right
         while (left <= right) {
+            
+            // 3. Find the mid safely
             int mid = left + (right - left) / 2;
 
+            // 4. The if statements
             if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
+                return mid; // Found it!
+            } 
+            else if (nums[mid] > target) {
+                // If the number at mid is too big, target is on the left
                 right = mid - 1;
+            } 
+            else {
+                // If the number at mid is too small, target is on the right
+                left = mid + 1; 
             }
         }
 
+        // 5. If nothing matches, left is standing exactly where the number belongs
         return left;
     }
 }
