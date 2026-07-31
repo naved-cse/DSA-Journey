f3b98e8b8ef9968df2b57ab63a5a6e1c31945076
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-07-31 22:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (32.30%) - LeetSync

--- Binary_Search/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ Binary_Search/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,42 +3,50 @@ class Solution {
         int firstPos = -1;
         int lastPos = -1;
 
+        // ==========================================
+        // LOOP 1: Find the FIRST position
+        // ==========================================
         int left = 0;
         int right = nums.length - 1;
 
-        while (left <= right){
+        while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target){
-                firstPos = mid;
+            if (nums[mid] == target) {
+                firstPos = mid;     // Save it!
+                right = mid - 1;    // Keep chopping off the right to hunt LEFT
+            } 
+            else if (nums[mid] > target) {
                 right = mid - 1;
-            }
-            else if (nums[mid] > target){
-                right = mid - 1;
-            }
+            } 
             else {
                 left = mid + 1;
             }
         }
 
+        // ==========================================
+        // LOOP 2: Find the LAST position
+        // ==========================================
+        // We MUST reset our pointers back to the start and end of the array!
         left = 0;
         right = nums.length - 1;
 
-        while (left <= right){
+        while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target){
-                lastPos = mid;
-                left = mid + 1;
-            }
-            else if (nums[mid] < target){
-                left = mid + 1;
-            }
-            else{
+            if (nums[mid] == target) {
+                lastPos = mid;      // Save it!
+                left = mid + 1;     // Keep chopping off the left to hunt RIGHT
+            } 
+            else if (nums[mid] > target) {
                 right = mid - 1;
+            } 
+            else {
+                left = mid + 1;
             }
         }
 
+        // Return our two answers!
         return new int[]{firstPos, lastPos};
     }
 }
