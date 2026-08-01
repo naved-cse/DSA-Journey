e665770fec392e1c6cc742389bc2453ba3592919
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-08-01 22:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (18.86%) - LeetSync

--- Binary_Search/162-find-peak-element/find-peak-element.java
+++ Binary_Search/162-find-peak-element/find-peak-element.java
@@ -3,16 +3,24 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left < right){
+        // We use < instead of <= because we use right = mid
+        while (left < right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] < nums[mid+1]){
+            // Look at the slope to our right
+            if (nums[mid] < nums[mid + 1]) {
+                // The ground goes UP! The peak must be to the right.
+                // mid is definitely NOT the peak, so we can throw it away.
                 left = mid + 1;
-            }
-            else{
+            } else {
+                // The ground goes DOWN! 
+                // The peak is either mid itself, or somewhere to the left.
                 right = mid;
             }
         }
-        return right;
+
+        // When the loop breaks, left and right are standing on the exact same spot.
+        // That spot is our guaranteed peak!
+        return left;
     }
 }
