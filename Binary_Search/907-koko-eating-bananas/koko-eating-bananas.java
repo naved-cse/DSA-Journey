4a924c2f04c4f05ab5ee7ac2e4049b8ef42cd9de
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-08-03 02:10:29 +0530

    Time: 8 ms (67.54%) | Memory: 48.5 MB (11.70%) - LeetSync

--- Binary_Search/907-koko-eating-bananas/koko-eating-bananas.java
+++ Binary_Search/907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,36 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
+        
         int left = 1;
         int right = 0;
 
-        for (int pile : piles) {
+        for (int pile : piles){
             right = Math.max(right, pile);
         }
 
-        while (left < right) {
+        while (left < right){
             int mid = left + (right - left) / 2;
 
-            if (canFinish(piles, h, mid)) {
+            long totalHours = 0;
+
+            for (int pile : piles){
+                totalHours += (pile + mid - 1) / mid;
+            }
+
+            if (totalHours <= h){
                 right = mid;
-            } else {
+            }
+            else{
                 left = mid + 1;
             }
         }
-
-        return left;
-    }
-
-    private boolean canFinish(int[] piles, int h, int speed) {
-        long hours = 0;
-
-        for (int pile : piles) {
-            hours += (pile + speed - 1) / speed;
-
-            if (hours > h) {
-                return false;
-            }
-        }
-
-        return true;
+        return right;
     }
 }
