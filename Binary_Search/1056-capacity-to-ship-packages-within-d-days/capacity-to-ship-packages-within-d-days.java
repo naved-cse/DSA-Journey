ed42ce9952340174f70e942a729ae97887540036
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-08-03 22:30:13 +0530

    Time: 9 ms (98.81%) | Memory: 50.5 MB (7.06%) - LeetSync

---
+++ Binary_Search/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int left = 0;
+        int right = 0;
+
+        for (int weight : weights){
+            left = Math.max(left, weight);
+            right += weight;
+        }
+
+        while (left < right){
+            int mid = left + (right - left) / 2;
+
+            int daysNeeded = 1;
+            int currentLoad = 0;
+
+            for (int weight : weights){
+                if (currentLoad + weight > mid){
+                    daysNeeded++;
+                    currentLoad = weight;
+                }
+                else{
+                    currentLoad += weight;
+                }
+            }
+
+            if (daysNeeded <= days){
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
