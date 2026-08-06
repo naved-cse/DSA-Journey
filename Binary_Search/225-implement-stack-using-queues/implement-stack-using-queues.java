5476d480cd5a149c7c4c1ab82d40632de27cd08a
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-08-07 02:02:07 +0530

    Time: 2 ms (26.75%) | Memory: 43 MB (17.56%) - LeetSync

--- Binary_Search/225-implement-stack-using-queues/implement-stack-using-queues.java
+++ Binary_Search/225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,31 +1,37 @@
-import java.util.*;
-
 class MyStack {
-
     Queue<Integer> q;
 
     public MyStack() {
         q = new LinkedList<>();
     }
-
+    
     public void push(int x) {
+        int size = q.size();
         q.offer(x);
-
-        // Rotate the queue so that the new element comes to the front
-        for (int i = 0; i < q.size() - 1; i++) {
-            q.offer(q.poll());
+        for(int i = 0; i < size; i++){
+            int personAtFront = q.remove();
+            q.add(personAtFront);
         }
     }
-
+    
     public int pop() {
         return q.poll();
     }
-
+    
     public int top() {
         return q.peek();
     }
-
+    
     public boolean empty() {
         return q.isEmpty();
     }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
