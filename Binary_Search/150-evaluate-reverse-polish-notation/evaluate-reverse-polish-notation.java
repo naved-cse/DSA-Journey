4ccde41a69f981d566b62a05c6ab179d5cadfa1b
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-08-07 23:12:37 +0530

    Time: 6 ms (87.22%) | Memory: 45.1 MB (78.98%) - LeetSync

--- Binary_Search/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ Binary_Search/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,36 +1,36 @@
-import java.util.Stack;
-
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
-
+        // We can use the modern ArrayDeque we talked about!
+        Deque<Integer> stack = new ArrayDeque<>();
+        
         for (String token : tokens) {
-            switch (token) {
-                case "+":
-                    stack.push(stack.pop() + stack.pop());
-                    break;
-
-                case "-":
-                    int b = stack.pop();
-                    int a = stack.pop();
-                    stack.push(a - b);
-                    break;
-
-                case "*":
-                    stack.push(stack.pop() * stack.pop());
-                    break;
-
-                case "/":
-                    b = stack.pop();
-                    a = stack.pop();
-                    stack.push(a / b); // Integer division truncates toward zero
-                    break;
-
-                default:
-                    stack.push(Integer.parseInt(token));
+            // If it's an operator, pop the top two numbers and do the math
+            if (token.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } 
+            else if (token.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } 
+            else if (token.equals("-")) {
+                // ORDER MATTERS! The first pop is the SECOND number.
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 - num2);
+            } 
+            else if (token.equals("/")) {
+                // ORDER MATTERS!
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 / num2);
+            } 
+            else {
+                // If it's not an operator, it MUST be a number!
+                // Convert the String to an int and push it.
+                stack.push(Integer.parseInt(token));
             }
         }
-
+        
+        // At the very end, the only thing left in the stack is the final answer
         return stack.pop();
     }
 }
