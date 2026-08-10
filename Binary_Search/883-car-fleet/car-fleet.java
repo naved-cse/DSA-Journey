a153052465176a05e8ec4137b802085e34bab116
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-08-10 20:42:17 +0530

    Time: 97 ms (19.31%) | Memory: 99.5 MB (42.24%) - LeetSync

--- Binary_Search/883-car-fleet/car-fleet.java
+++ Binary_Search/883-car-fleet/car-fleet.java
@@ -1,36 +1,26 @@
-import java.util.Arrays;
-
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
-        if (n == 0) return 0;
-        
-        // Combine position and speed into a 2D array
-        int[][] cars = new int[n][2];
-        for (int i = 0; i < n; i++) {
+
+        double[][] cars = new double[n][2];
+        for (int i = 0; i < n; i++){
             cars[i][0] = position[i];
             cars[i][1] = speed[i];
         }
-        
-        // Sort the cars by position in descending order (closest to target first)
-        Arrays.sort(cars, (a, b) -> Integer.compare(b[0], a[0]));
-        
-        int fleets = 0;
-        double maxTimeSeen = 0.0;
-        
-        // Iterate from the car closest to the target to the furthest
-        for (int i = 0; i < n; i++) {
-            // Calculate time to reach the target for the current car
-            double timeToTarget = (double) (target - cars[i][0]) / cars[i][1];
-            
-            // If this car's time is strictly greater than the max time seen so far,
-            // it will never catch up to the fleet ahead of it. It forms a new fleet.
-            if (timeToTarget > maxTimeSeen) {
-                fleets++;
-                maxTimeSeen = timeToTarget;
+        Arrays.sort(cars, (a,b) -> Double.compare(a[0], b[0]));
+
+        Deque<Double> stack = new ArrayDeque<>();
+
+        for (int i = n - 1; i >= 0; i--){
+            double time = (target - cars[i][0]) / cars[i][1];
+
+            if (stack.isEmpty()){
+                stack.push(time);
+            }
+            else if (time > stack.peek()){
+                stack.push(time);
             }
         }
-        
-        return fleets;
+        return stack.size();
     }
 }
