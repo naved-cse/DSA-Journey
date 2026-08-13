9fc27cf9caf04b045144e529305bb1e1a8253f53
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-08-13 22:30:45 +0530

    Time: 2 ms (98.04%) | Memory: 46.7 MB (83.83%) - LeetSync

--- Binary_Search/735-asteroid-collision/asteroid-collision.java
+++ Binary_Search/735-asteroid-collision/asteroid-collision.java
@@ -1,44 +1,32 @@
-import java.util.Stack;
-
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        for (int asteroid : asteroids) {
-            boolean destroyed = false;
-            
-            // Process collisions: Stack has a right-moving asteroid, current is left-moving
-            while (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0) {
-                int currentSize = Math.abs(asteroid);
-                
-                if (stack.peek() < currentSize) {
-                    // The stack asteroid is smaller and explodes. 
-                    // Continue the loop to check the next asteroid in the stack.
-                    stack.pop();
-                    continue;
-                } else if (stack.peek() == currentSize) {
-                    // Both asteroids are the same size and explode.
+        for (int ast : asteroids){
+            boolean survived = true;
+
+            while (!stack.isEmpty() && stack.peek() > 0 && ast < 0){
+                if (Math.abs(ast) > stack.peek()){
                     stack.pop();
                 }
-                
-                // If we reach here, the current asteroid was either equal to 
-                // or smaller than the stack asteroid, so it is destroyed.
-                destroyed = true;
-                break;
+                else if (Math.abs(ast) == stack.peek()){
+                    stack.pop();
+                    survived = false;
+                    break;
+                }
+                else if (Math.abs(ast) < stack.peek()){
+                    survived = false;
+                    break;
+                }
             }
-            
-            // If the current asteroid survived all collisions, add it to the stack
-            if (!destroyed) {
-                stack.push(asteroid);
+            if (survived){
+                stack.push(ast);
             }
         }
-
-        // Convert the remaining stack back into an array
         int[] result = new int[stack.size()];
-        for (int i = result.length - 1; i >= 0; i--) {
+        for (int i = result.length - 1; i >= 0; i--){
             result[i] = stack.pop();
         }
-        
         return result;
     }
 }
