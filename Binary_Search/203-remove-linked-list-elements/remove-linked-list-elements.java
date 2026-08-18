02db0e8d52a9ac3f46fa0a2aaf43d0b2868db276
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-08-18 22:02:29 +0530

    Time: 1 ms (96.95%) | Memory: 47.3 MB (40.74%) - LeetSync

--- Binary_Search/203-remove-linked-list-elements/remove-linked-list-elements.java
+++ Binary_Search/203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -10,26 +10,19 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        // Create a dummy node that points to the head of the list.
-        ListNode dummy = new ListNode(-1);
+        ListNode dummy = new ListNode(0);
         dummy.next = head;
-        
-        // Use a current pointer starting at the dummy node.
-        ListNode curr = dummy;
-        
-        // Traverse the list
-        while (curr.next != null) {
-            // If the next node needs to be removed
-            if (curr.next.val == val) {
-                // Skip the next node
-                curr.next = curr.next.next;
-            } else {
-                // Otherwise, just move the pointer forward
-                curr = curr.next;
+
+        ListNode current = dummy;
+
+        while (current.next != null){
+            if (current.next.val == val){
+                current.next = current.next.next;
+            }
+            else{
+                current = current.next;
             }
         }
-        
-        // Return the new head, which is the node after the dummy
         return dummy.next;
     }
 }
