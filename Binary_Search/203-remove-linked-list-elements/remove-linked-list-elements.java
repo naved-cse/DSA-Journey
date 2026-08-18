3414b23a23c9448414b9ed421d00fc0c2188eddf
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-08-18 22:02:42 +0530

    Time: 1 ms (96.95%) | Memory: 47.7 MB (7.74%) - LeetSync

--- Binary_Search/203-remove-linked-list-elements/remove-linked-list-elements.java
+++ Binary_Search/203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -1,28 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        
+        // 1. Create the dummy node and point it to the head
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-
+        
+        // 2. Start our walker AT THE DUMMY NODE
         ListNode current = dummy;
-
-        while (current.next != null){
-            if (current.next.val == val){
+        
+        // 3. Loop as long as there is a node in front of us to check
+        while (current.next != null) {
+            
+            // 4. Look one step ahead: Is the NEXT node the target?
+            if (current.next.val == val) {
+                // BYPASS ALGORITHM: 
+                // Skip the next node by pointing to the one after it!
                 current.next = current.next.next;
-            }
-            else{
+                
+                // (Notice we DO NOT move 'current' forward here. 
+                // We stay put just in case the new next node is also the target!)
+            } 
+            else {
+                // Safe node! We can safely take a step forward.
                 current = current.next;
             }
         }
+        
+        // 5. Return the real list (ignoring the dummy)
         return dummy.next;
     }
 }
