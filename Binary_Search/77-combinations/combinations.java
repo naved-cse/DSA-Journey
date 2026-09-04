5d0a550043e30d7e680b61438f0d04d77bed80d0
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-09-04 22:32:58 +0530

    Time: 18 ms (79.43%) | Memory: 97.8 MB (82.10%) - LeetSync

--- Binary_Search/77-combinations/combinations.java
+++ Binary_Search/77-combinations/combinations.java
@@ -1,25 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(1, n, k, new ArrayList<>(), result);
+        List<Integer> bucket = new ArrayList<>();
+
+        backtrack(result, bucket, 1, n, k);
+
         return result;
     }
 
-    private void backtrack(int start, int n, int k, List<Integer> current, List<List<Integer>> result) {
-        // Base case: combination is complete
-        if (current.size() == k) {
-            result.add(new ArrayList<>(current));
+    private void backtrack(List<List<Integer>> result, List<Integer> bucket, int start, int n , int k) {
+        if (bucket.size() == k){
+            result.add(new ArrayList<>(bucket));
             return;
         }
+        for (int i = start; i <= n; i++){
+            bucket.add(i);
 
-        // Optimization: stop if there aren't enough numbers left to complete a combination of size k
-        for (int i = start; i <= n - (k - current.size()) + 1; i++) {
-            current.add(i);
-            backtrack(i + 1, n, k, current, result);
-            current.remove(current.size() - 1); // backtrack
+            backtrack(result, bucket, i+1, n, k);
+
+            bucket.remove(bucket.size() - 1);
         }
     }
 }
