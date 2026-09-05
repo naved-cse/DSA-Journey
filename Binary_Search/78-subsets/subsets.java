89e86d770f81412feb98eb580989055567086e47
Kazi Naved Akhtar <iamnavedkazi@gmail.com>
2026-09-05 21:00:18 +0530

    Time: 1 ms (86.70%) | Memory: 44.5 MB (6.75%) - LeetSync

--- Binary_Search/78-subsets/subsets.java
+++ Binary_Search/78-subsets/subsets.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        // Start backtracking from index 0 with an empty current list
-        backtrack(result, new ArrayList<>(), nums, 0);
-        return result;
+        List<Integer> bucket = new ArrayList<>();
+
+        backtrack(result, bucket, nums, 0);
+
+        return result; 
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, int start) {
-        // Add a copy of the current subset to the result
-        result.add(new ArrayList<>(current));
+    private void backtrack(List<List<Integer>> result, List<Integer>bucket, int[] nums, int start) {
 
-        // Iterate through the remaining elements
-        for (int i = start; i < nums.length; i++) {
-            // Include the current element
-            current.add(nums[i]);
-            
-            // Move to the next element
-            backtrack(result, current, nums, i + 1);
-            
-            // Backtrack: remove the last added element to explore other paths
-            current.remove(current.size() - 1);
+        result.add(new ArrayList<>(bucket));
+
+        for (int i = start; i < nums.length; i++){
+        bucket.add(nums[i]);
+
+        backtrack(result, bucket, nums, i + 1);
+
+        bucket.remove(bucket.size() - 1);
         }
     }
 }
